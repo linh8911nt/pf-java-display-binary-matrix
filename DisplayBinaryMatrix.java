b2dc1b51247ed611a7bf109856f75e28da7081cb
linh8911nt <thanhlinh12314@gmail.com>
2018-03-28 10:34:21 +0700

    pf-java-display-binary-matrix

--- DisplayBinaryMatrix.java
+++ DisplayBinaryMatrix.java
@@ -9,15 +9,15 @@ public class DisplayBinaryMatrix {
         printMatrix(sizeMatrix);
     }
 
-    public static void printMatrix(int sizeMatrix){
+    public static void printMatrix(int sizeMatrix) {
         int[][] matrix = new int[sizeMatrix][sizeMatrix];
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[0].length; j++){
-                matrix[i][j] = (int) Math.round(Math.random()*1);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = (int) Math.round(Math.random() * 1);
             }
         }
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[0].length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
